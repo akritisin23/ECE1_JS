fce1da305d3b05da71bb93d439c039397ca2996a
akritisin23 <akritisingh98998@gmail.com>
2026-03-18 10:22:58 +0530

    dom.js

---
+++ dom.js
@@ -0,0 +1,34 @@
+const result = document.getElementById("as");
+result.innerHTML = "<b>Hello World</b>";
+//query selector
+//const result2 = document.querySelector("#as");
+//result2.innerHTML = "<b>Hello World</b>";
+
+
+//event
+//click
+//mouseover=onmouseover
+//mouseout=onmouseout
+//mousedown=onmousedown
+//mouseup=onmouseup
+//mousemove=onmousemove
+//mousedoubleclick=ondblclick
+
+function onClick(){
+    console.log("on mouse out")
+}
+function onMouseDown(){
+    console.log("on mouse down")
+}
+function onMouseUp(){
+    console.log("on mouse up")
+}
+function onMouseOver(){
+    console.log("on mouse over")
+}
+function onMouseOut(){
+    console.log("on mouse out")
+}
+function onMouseMove(){
+    console.log("on mouse move")
+}
